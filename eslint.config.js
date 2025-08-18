f6e21a4ef103b67568489f9cd3a9cd3ceb84e7f8
Gayrat Vlasov <ggvlasov@gmail.com>
2025-08-18 21:47:55 +0300

    eslint убрал контроль any

--- eslint.config.js
+++ eslint.config.js
@@ -56,6 +56,7 @@ export default defineConfig([
             "no-unused-vars": "off",
             "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
             "no-console": ["warn", { allow: ["warn", "error"] }],
+            "@typescript-eslint/no-explicit-any": "off"
         },
     },
 ]);
